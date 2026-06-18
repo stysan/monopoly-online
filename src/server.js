915db8f3a42c38275a7ffa1362f950e08b21b355
stysan <55314369+stysan@users.noreply.github.com>
2026-06-18 22:14:54 +0300

    Change server port to 10498

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
   cors: { origin: '*', methods: ['GET', 'POST'] }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10498;
 
 app.use(express.json());
 app.use(cookieParser());
